5c861827d160578daee34340eef40bf57d761cce
LuisDMirabal <54773051+LuisDMirabal@users.noreply.github.com>
2019-09-20 10:04:53 -0400

    Add files via upload

---
+++ Encrypt.java
@@ -0,0 +1,29 @@
+package mirabalp1;
+
+import java.util.Scanner;
+
+public class Encrypt {
+
+	public static void main(String[] args) 
+	{
+		Scanner scnr = new Scanner(System.in);
+		
+		System.out.print("Please enter number to be encrypted: ");
+		
+		int number = scnr.nextInt();
+		
+		int encrypted;
+		int thousand = ((number / 1000) +7) % 10;
+		int hundred = ((number / 100) + 7) % 10;
+		int ten = ((number / 10) + 7) % 10;
+		int one = (number + 7) % 10;
+		
+		encrypted = (ten * 1000) + (one * 100) + (thousand * 10) + hundred;
+		
+		System.out.println(encrypted);
+		
+		scnr.close(); 
+
+	}
+
+}
